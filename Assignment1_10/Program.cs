2faf489b243c3405f26b591897a64e695ffcd5c8
YGumarov <yerassylgumarov@gmail.com>
2023-04-09 15:40:06 +0600

    Initial commit

---
+++ Assignment1_10/Program.cs
@@ -0,0 +1,16 @@
+﻿ static int gcd(int a, int b)
+{
+    if (b == 0)
+    {
+        return a;
+    }
+    else
+    {
+        return gcd(b, a % b);
+    }
+}
+
+int a = int.Parse(Console.ReadLine());
+int b = int.Parse(Console.ReadLine());
+
+Console.WriteLine(gcd(a,b));
